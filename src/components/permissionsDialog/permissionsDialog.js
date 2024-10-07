fefdd4e955806c0fcc75181d7451392b6c923164
NoahOriano <noahoriano@gmail.com>
2024-10-07 17:07:21 +0200

    feat: retrieve users and UI for user perm management

--- src/components/permissionsDialog/permissionsDialog.js
+++ src/components/permissionsDialog/permissionsDialog.js
@@ -13,7 +13,7 @@ import '../formdialog.scss';
 import ServerConnections from '../ServerConnections';
 import toast from '../toast/toast';
 
-function getEditorHtml() {
+function getEditorHeaderHtml() {
     let html = '';
 
     html += '<div class="formDialogContent smoothScrollY" style="padding-top:2em;">';
@@ -21,26 +21,21 @@ function getEditorHtml() {
     html += '<form style="margin:auto;">';
 
     html += '<div class="fldSelectPlaylist selectContainer">';
-    html += '<select is="emby-select" id="selectMetadataRefreshMode" label="' + globalize.translate('SetVisibility') + '">';
-    html += '<option value="scan" selected>' + globalize.translate('MakePrivate') + '</option>';
-    html += '<option value="missing">' + globalize.translate('MakePublic') + '</option>';
+    html += '<select is="emby-select" id="selectViewPermissions" label="' + globalize.translate('SetVisibility') + '">';
+    html += '<option value="private" selected>' + globalize.translate('MakePrivate') + '</option>';
+    html += '<option value="public">' + globalize.translate('MakePublic') + '</option>';
     html += '</select>';
     html += '</div>';
-
-    html += '<label class="checkboxContainer hide fldReplaceExistingImages">';
-    html += '<input type="checkbox" is="emby-checkbox" class="chkReplaceImages" />';
-    html += '<span>' + globalize.translate('ViewPermissions') + '</span>';
-    html += '</label>';
-
-    html += '<label class="checkboxContainer hide fldReplaceTrickplayImages">';
-    html += '<input type="checkbox" is="emby-checkbox" class="chkReplaceTrickplayImages" />';
-    html += '<span>' + globalize.translate('EditPermissions') + '</span>';
-    html += '</label>';
 
     html += '<div class="fieldDescription">';
     html += globalize.translate('PlaylistPublicPrivateInfo');
     html += '</div>';
 
+    return html;
+}
+
+function getEditorFooterHtml() {
+    let html = '';
     html += '<input type="hidden" class="fldSelectedItemIds" />';
 
     html += '<br />';
@@ -52,6 +47,25 @@ function getEditorHtml() {
     html += '</div>';
     html += '</div>';
 
+    return html;
+}
+
+function getUserPermssionHtml(user) {
+    let html = '';
+    html += '<div class="userPermissions ' + user.Name + '">';
+    html += '<div class="userPermissionHeader">';
+    html += user.Name;
+    html += '</div>';
+    html += '<label class="checkboxContainer fldViewPermissions">';
+    html += '<input type="checkbox" is="emby-checkbox" class="chkViewPermissions" />';
+    html += '<span>' + globalize.translate('ViewPermissions') + '</span>';
+    html += '</label>';
+
+    html += '<label class="checkboxContainer fldEditPermissions">';
+    html += '<input type="checkbox" is="emby-checkbox" class="chkEditPermissions" />';
+    html += '<span>' + globalize.translate('EditPermissions') + '</span>';
+    html += '</label>';
+    html += '</div>';
     return html;
 }
 
@@ -71,9 +85,9 @@ function onSubmit(e) {
 
     const apiClient = ServerConnections.getApiClient(options.serverId);
 
-    const replaceAllMetadata = dlg.querySelector('#selectMetadataRefreshMode').value === 'all';
+    const replaceAllMetadata = dlg.querySelector('#selectViewPermissions').value === 'all';
 
-    const mode = dlg.querySelector('#selectMetadataRefreshMode').value === 'scan' ? 'Default' : 'FullRefresh';
+    const mode = dlg.querySelector('#selectViewPermissions').value === 'private' ? 'Default' : 'FullRefresh';
     const replaceAllImages = mode === 'FullRefresh' && dlg.querySelector('.chkReplaceImages').checked;
     const replaceTrickplayImages = mode === 'FullRefresh' && dlg.querySelector('.chkReplaceTrickplayImages').checked;
 
@@ -98,12 +112,53 @@ function onSubmit(e) {
     return false;
 }
 
+// Permissions Dialog for individual playlist permissions
 class PermissionsDialog {
     constructor(options) {
         this.options = options;
+        this.itemId = this.options.itemIds[0]; // itemIds is an array with one item
+        this.serverId = this.options.serverId;
+        this.apiClient = ServerConnections.getApiClient(this.serverId);
     }
 
-    show() {
+    // Gets a list of all users then also gets their permissions for this playlist
+    async getAllUsers() {
+        // Create a list of all users
+        const allUsers = await this.apiClient.getUsers();
+        console.log(allUsers);
+        if (allUsers != 'undefined') {
+            if (allUsers.length === 0) {
+                return 'undefined';
+            } else {
+                for (const user of allUsers) {
+                    user.permissions = this.apiClient.getJSON('Playlists/' + this.itemId + '/Users/' + user.Id);
+                }
+            }
+        }
+        // const currentUser = this.apiClient.getCurrentUser();
+        // Filter out the current user
+        return allUsers; // .filter((user) => user.id !== currentUser.Id);
+    }
+
+    // Update the permissions for a specific user
+    // Entering a null permissions object will remove the user's permissions
+    updateUserPermissions(user, permissions) {
+        user.modified = true;
+        user.permissions = permissions;
+    }
+
+    // Sends the request to update the permissions for a specific user
+    // Seperate from updateUserPermissions to allow for batch updating and performance improvements
+    sendUpdateRequest(userId, permissions) {
+        return this.apiClient.ajax({
+            type: 'POST',
+            url: 'Playlists/' + this.itemId + '/Users/' + userId,
+            data: JSON.stringify(permissions),
+            contentType: 'application/json'
+        });
+    }
+
+    async show() {
         const dialogOptions = {
             removeOnClose: true,
             scrollY: false
@@ -127,30 +182,67 @@ class PermissionsDialog {
         html += '<h3 class="formDialogHeaderTitle">';
         html += title;
         html += '</h3>';
-
         html += '</div>';
+        html += getEditorHeaderHtml();
 
-        html += getEditorHtml();
+        const users = await this.getAllUsers();
+        if (users != 'undefined') {
+            for (const user of users ) {
+                html += getUserPermssionHtml(user);
+            }
+        } else {
+            html += '<div class="fieldDescription">';
+            html += globalize.translate('NoUsersFound');
+            html += '</div>';
+        }
+        html += getEditorFooterHtml();
 
         dlg.innerHTML = html;
 
         dlg.querySelector('form').addEventListener('submit', onSubmit.bind(this));
 
-        dlg.querySelector('#selectMetadataRefreshMode').addEventListener('change', function () {
-            if (this.value === 'scan') {
-                dlg.querySelector('.fldReplaceExistingImages').classList.add('hide');
-                dlg.querySelector('.fldReplaceTrickplayImages').classList.add('hide');
+        // Adds listeners to the checkboxes to show/hide the edit permissions field
+        dlg.querySelectorAll('.chkViewPermissions').forEach(function (chkViewPermissions) {
+            chkViewPermissions.addEventListener('change', function () {
+                const userHTML = dom.parentWithClass(chkViewPermissions, 'userPermissions');
+                const hasViewPermissions = chkViewPermissions.checked;
+                if (!hasViewPermissions) {
+                    if (!userHTML.querySelector('.fldEditPermissions').classList.contains('hide')) {
+                        userHTML.querySelector('.fldEditPermissions').classList.add('hide');
+                    }
+                } else if (userHTML.querySelector('.fldEditPermissions').classList.contains('hide')) {
+                    userHTML.querySelector('.fldEditPermissions').classList.remove('hide');
+                }
+            });
+        });
+
+        // Adds listener to the public/private selection to show/hide the permissions fields
+        dlg.querySelector('#selectViewPermissions').addEventListener('change', function () {
+            if (this.value === 'private') {
+                dlg.querySelectorAll('.userPermissions').forEach(function (userHTML) {
+                    userHTML.querySelector('.fldViewPermissions').classList.remove('hide');
+                    const hasViewPermissions = userHTML.querySelector('.chkViewPermissions').checked;
+                    if (!hasViewPermissions) {
+                        if (!userHTML.querySelector('.fldEditPermissions').classList.contains('hide')) {
+                            userHTML.querySelector('.fldEditPermissions').classList.add('hide');
+                        }
+                    } else if (userHTML.querySelector('.fldEditPermissions').classList.contains('hide')) {
+                        userHTML.querySelector('.fldEditPermissions').classList.remove('hide');
+                    }
+                });
             } else {
-                dlg.querySelector('.fldReplaceExistingImages').classList.remove('hide');
-                dlg.querySelector('.fldReplaceTrickplayImages').classList.remove('hide');
+                dlg.querySelectorAll('.userPermissions').forEach(function (userHTML) {
+                    userHTML.querySelector('.fldViewPermissions').classList.add('hide');
+                    userHTML.querySelector('.fldEditPermissions').classList.remove('hide');
+                });
             }
         });
 
         if (this.options.mode) {
-            dlg.querySelector('#selectMetadataRefreshMode').value = this.options.mode;
+            dlg.querySelector('#selectViewPermissions').value = this.options.mode;
         }
 
-        dlg.querySelector('#selectMetadataRefreshMode').dispatchEvent(new CustomEvent('change'));
+        dlg.querySelector('#selectViewPermissions').dispatchEvent(new CustomEvent('change'));
 
         dlg.querySelector('.btnCancel').addEventListener('click', function () {
             dialogHelper.close(dlg);
